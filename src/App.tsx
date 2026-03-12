d35d62eae7fc35b4af65e67ce0ff2bf42488fb45
george <heorhii.kyryliuk@gmail.com>
2026-03-12 22:06:39 +0200

    init, add packages, config dev deps & files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Hello Cast Roam Fisherman Finds</Text>
+    </View>
+  );
+}
+
+export default App;
