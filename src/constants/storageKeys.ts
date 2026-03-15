c166b1c0f40083118f5ab1d969a57d2113c564be
george <heorhii.kyryliuk@gmail.com>
2026-03-15 20:17:14 +0200

    small fixes

--- src/constants/storageKeys.ts
+++ src/constants/storageKeys.ts
@@ -1,3 +1,3 @@
-export const STORAGE_KEYS = ['is_onboarding_completed'] as const;
+export const STORAGE_KEYS = ['is_onboarding_completed', 'saved_tips'] as const;
 
 export type StorageKey = (typeof STORAGE_KEYS)[number];
